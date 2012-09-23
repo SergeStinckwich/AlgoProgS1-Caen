2271d0c688eeccb1ad0d3d6b1ff3f1a0923f5a13
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-09-23 21:14:23 +0200

    First commit

---
+++ tp1/exo1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+  int dayOfBirth, monthOfBirth, yearOfBirth;
+  int dayOfToday, monthOfToday, yearOfToday;
+
+  printf("Quelle est votre date de naissance (jour/mois/annee) ? ");
+  scanf("%d / %d / %d",&dayOfBirth,&monthOfBirth,&yearOfBirth);
+  printf("Quelle est la date d'aujourd'hui (jour/mois/annee) ? ");
+  scanf("%d / %d / %d",&dayOfToday,&monthOfToday,&yearOfToday);
+  if(monthOfToday>monthOfBirth || (monthOfToday == monthOfBirth && dayOfToday >= dayOfBirth)){
+    printf("\nVous avez %d ans.\n",yearOfToday-yearOfBirth);
+  }
+  else {
+    printf("\nVous avez %d ans.\n", yearOfToday-yearOfBirth-1);
+  }
+}
