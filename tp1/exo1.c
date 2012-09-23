edbdfc65269080b0c439775970875a2aa56517d2
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-09-23 21:28:18 +0200

    All exercices of TP1

--- tp1/exo1.c
+++ tp1/exo1.c
@@ -5,9 +5,9 @@ int main(void){
   int dayOfToday, monthOfToday, yearOfToday;
 
   printf("Quelle est votre date de naissance (jour/mois/annee) ? ");
-  scanf("%d / %d / %d",&dayOfBirth,&monthOfBirth,&yearOfBirth);
+  scanf("%d / %d / %d", &dayOfBirth, &monthOfBirth, &yearOfBirth);
   printf("Quelle est la date d'aujourd'hui (jour/mois/annee) ? ");
-  scanf("%d / %d / %d",&dayOfToday,&monthOfToday,&yearOfToday);
+  scanf("%d / %d / %d", &dayOfToday, &monthOfToday, &yearOfToday);
   if(monthOfToday>monthOfBirth || (monthOfToday == monthOfBirth && dayOfToday >= dayOfBirth)){
     printf("\nVous avez %d ans.\n",yearOfToday-yearOfBirth);
   }
