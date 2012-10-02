30b517b0daa2095cc0f4c228ac837a3e848d7e51
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-10-02 17:36:57 +0200

    Exercice pyramide suite

---
+++ tp2/pyramide.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+int nbLines, n;
+printf("Entrer un nombre de lignes pour la pyramide=");
+scanf("%d", &nbLines);
+n = nbLines;
+for (int i=0; i<nbLines*2; i=i+2)
+	{
+		for (int j=0; j<=n; j++)
+			printf(" ");
+		n --;
+		for(int j=0; j<=i; j++)
+			printf("%d", j);
+		printf("\n");
+	}
+}
