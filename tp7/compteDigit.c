cef4301f6ac517d045b4c157365a168c407011d9
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-10-22 16:00:22 +0200

    Add compteDigit

--- tp7/compteDigit.c
+++ tp7/compteDigit.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int estUnChiffre(char c){
 // Retourne vrai si c est un chiffre
 // faux sinon
@@ -17,16 +16,19 @@ int nombreChiffres(char a []){
 	return nbChiffres;
 }
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-	char ch1[6] = "Hello\0";
-	char ch2[6] = "H1ll0\0";
-	char ch3[11] = "0123456789\0";
+	char ch1[1]  = "\0";
+	char ch2[6]  = "Hello\0";
+	char ch3[6]  = "H1ll0\0";
+	char ch4[11] = "0123456789\0";
+
 	printf("%d\n", estUnChiffre('1')); // 1
 	printf("%d\n", estUnChiffre('X')); // 0
 
 	printf("%d\n", nombreChiffres(ch1)); // 0
-	printf("%d\n", nombreChiffres(ch2)); // 2
-	printf("%d\n", nombreChiffres(ch3)); // 10
+	printf("%d\n", nombreChiffres(ch2)); // 0
+	printf("%d\n", nombreChiffres(ch3)); // 2
+	printf("%d\n", nombreChiffres(ch4)); // 10
 	return 0;
 }
